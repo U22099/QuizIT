56b311e90c5e33b319a59624ff4370db0558d780
U22099 <nifemiolaniyi4@gmail.com>
2024-12-30 20:22:16 +0100

    update TextInput.tsx

--- src/components/home/components/TextInput.tsx
+++ src/components/home/components/TextInput.tsx
@@ -9,7 +9,7 @@ export function TextInput({ setData }: TextInputProps ): JSX.Element{
     <textarea
       className="flex justify-center items-center border rounded-md p-2 text-md w-40 h-48"
       placeholder="Paste/Type in a sample question set in the format below\n1\tquestion...\n2\tquestion..."
-      onChange={(e: React.ChangeEvent<HTMLInputElement>) => setData(e.target.value || "")}
+      onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setData(e.target.value || "")}
     />
   )
 }
