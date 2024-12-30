23110e40d81bf83fc0518f301c5a0c312c806f6b
U22099 <nifemiolaniyi4@gmail.com>
2024-12-30 22:10:45 +0100

    update

--- src/components/home/components/InputNav.tsx
+++ src/components/home/components/InputNav.tsx
@@ -8,7 +8,7 @@ interface InputNavProps{
 }
 
 export function InputNav({ nav, setNav }: InputNavProps): JSX.Element{
-  const custom_style = " flex justify-center items-center p-2 rounded-md w-8 h-8";
+  const custom_style = " flex justify-center items-center p-2 rounded-md w-9 h-9";
   return(
     <nav className="bg-primary flex flex-col gap-2 p-2 rounded-md h-fit border my-auto">
       <FaPencil onClick={() => setNav("text")} className={nav === "text" ? "bg-black dark:bg-white fill-white dark:fill-black"+custom_style : "fill-black dark:fill-white"+custom_style} />
--- src/components/home/components/TextInput.tsx
+++ src/components/home/components/TextInput.tsx
@@ -7,7 +7,7 @@ interface TextInputProps {
 export function TextInput({ setData }: TextInputProps ): JSX.Element{
   return(
     <textarea
-      className="flex justify-center items-center border rounded-md p-2 text-sm w-60 h-52 active:outline-none border bg-transparent backdrop-blur-sm"
+      className="flex justify-center items-center border rounded-md p-2 text-sm w-64 h-56 active:outline-none border bg-transparent backdrop-blur-sm"
       placeholder="Paste/Type in a sample question set"
       onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setData(e.target.value || "")}
     />
