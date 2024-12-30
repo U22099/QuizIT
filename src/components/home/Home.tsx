af7f0ac4ae97ffb740336f91259b2635a0a23a69
U22099 <nifemiolaniyi4@gmail.com>
2024-12-30 21:20:51 +0100

    update Home.tsx

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -8,7 +8,7 @@ export function Home(): JSX.Element{
   const [ nav, setNav ] = useState<string>("text");
   return(
     <main className="bg-theme flex flex-col gap-2 justify-start items-center mt-8">
-      <section className="flex gap-2 w-fit">
+      <section className="flex gap-2 w-fit mx-auto">
         {nav === "text" ? <TextInput setData={setData} /> : <FileInput setData={setData} />}
         <InputNav setNav={setNav} nav={nav} />
       </section>
