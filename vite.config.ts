50f14a9246efb6b7eedf2d59134e56195362c798
U22099 <nifemiolaniyi4@gmail.com>
2025-01-01 01:56:33 +0100

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,40 +6,43 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   base: "/QuizIT/",
   plugins: [react(), VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'QuizIT: Digital Exam Generator',
-        short_name: 'QuizIT',
-        description: 'An AI powered exam generator using sample question inputs and providing a simple digital examination environment with timer and ai calculated answers',
-        background_color: 'black',
-        display: 'standalone',
-        start_url: '/',
-        scope: '/QuizIT',
-        orientation: 'portrait',
-        theme_color: 'black',
-        icons: [
-          {
-            src: "pwa-64x64.png",
-            sizes: "64x64",
-            type: "image/png"
+    workbox: {
+      maximumFileSizeToCacheInBytes: 10485760
+    },
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'QuizIT: Digital Exam Generator',
+      short_name: 'QuizIT',
+      description: 'An AI powered exam generator using sample question inputs and providing a simple digital examination environment with timer and ai calculated answers',
+      background_color: 'black',
+      display: 'standalone',
+      start_url: '/',
+      scope: '/QuizIT',
+      orientation: 'portrait',
+      theme_color: 'black',
+      icons: [
+        {
+          src: "pwa-64x64.png",
+          sizes: "64x64",
+          type: "image/png"
 		},
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
+        {
+          src: "pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png"
 		},
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png"
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png"
   		},
-          {
-            src: "maskable-icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable"
+        {
+          src: "maskable-icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable"
   		}
 	]
-      }
-    })],
+    }
+  })],
 });
